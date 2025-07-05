542b652b7ee7d8c245cb464240cdbd260aabbca8
thevpc <taha.bensalah@gmail.com>
2025-07-05 03:33:25 +0100

    [UPDATED] NApp.Main renamed to NApp.Runner

--- ndb/src/main/java/net/thevpc/ndb/NDdbMain.java
+++ ndb/src/main/java/net/thevpc/ndb/NDdbMain.java
@@ -3,8 +3,6 @@ package net.thevpc.ndb;
 import net.thevpc.ndb.cmd.options.NDdbOptionsParser;
 import net.thevpc.ndb.cmd.NDdbRunner;
 import net.thevpc.nuts.NApp;
-import net.thevpc.nuts.NApplication;
-import net.thevpc.nuts.NMainArgs;
 
 @NApp.Info
 public class NDdbMain  {
@@ -12,7 +10,7 @@ public class NDdbMain  {
         NApp.builder(args).run();
     }
 
-    @NApp.Main
+    @NApp.Runner
     public void run() {
         NDdbRunner.run(NDdbOptionsParser.parse(NApp.of().getCmdLine()));
     }

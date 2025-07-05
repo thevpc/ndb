7549f9e883a1f270c88fa0029f4b336b19503822
thevpc <taha.bensalah@gmail.com>
2025-07-05 21:43:27 +0100

    [CHANGED] changed NApp.Definition to NAppDefinition

--- ndb/src/main/java/net/thevpc/ndb/NDdbMain.java
+++ ndb/src/main/java/net/thevpc/ndb/NDdbMain.java
@@ -3,14 +3,16 @@ package net.thevpc.ndb;
 import net.thevpc.ndb.cmd.options.NDdbOptionsParser;
 import net.thevpc.ndb.cmd.NDdbRunner;
 import net.thevpc.nuts.NApp;
+import net.thevpc.nuts.NAppDefinition;
+import net.thevpc.nuts.NAppRunner;
 
-@NApp.Definition
+@NAppDefinition
 public class NDdbMain  {
     public static void main(String[] args) {
         NApp.builder(args).run();
     }
 
-    @NApp.Runner
+    @NAppRunner
     public void run() {
         NDdbRunner.run(NDdbOptionsParser.parse(NApp.of().getCmdLine()));
     }

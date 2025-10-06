1574071711f752f7cf89563d939741cae266325d
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:20:15 +0100

    Update NMongoSupport.java : bump nuts 0.8.7

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/nosql/mongodb/NMongoSupport.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/nosql/mongodb/NMongoSupport.java
@@ -6,8 +6,8 @@
 package net.thevpc.ndb.servers.nosql.mongodb;
 
 import com.mongodb.client.*;
-import net.thevpc.nuts.*;
 import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.ndb.servers.NdbConfig;
 import net.thevpc.ndb.servers.base.CmdRedirect;

740055b3aa4a6431a46fae3c8d15e18532aa564c
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:20:15 +0100

    Update SqlRunnable.java : bump nuts 0.8.7

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/util/SqlRunnable.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/util/SqlRunnable.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndb.servers.sql.util;
 
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NSession;
 
 public interface SqlRunnable {
     void run(SqlHelper h, NSession session) throws Exception;

b80716e6de439f5b7fab95eacd93076480486746
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:20:15 +0100

    Update SqlCallable.java : bump nuts 0.8.7

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/util/SqlCallable.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/util/SqlCallable.java
@@ -1,6 +1,6 @@
 package net.thevpc.ndb.servers.sql.util;
 
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NSession;
 
 public interface SqlCallable<T> {
     T run(SqlHelper h, NSession session) throws Exception;

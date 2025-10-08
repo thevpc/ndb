5bfcf3a3a06a36280d65538505234cc729e2d7dc
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:53 +0100

    Update RemoteMysqlDatabaseConfigService.java : refactor NMsg

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/nmysql/remote/RemoteMysqlDatabaseConfigService.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/nmysql/remote/RemoteMysqlDatabaseConfigService.java
@@ -37,7 +37,7 @@ import net.thevpc.ndb.servers.sql.nmysql.util.MysqlUtils;
 import net.thevpc.ndb.servers.util.NdbUtils;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.io.NAsk;
 
 public class RemoteMysqlDatabaseConfigService {

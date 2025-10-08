908535a1fda7c87b4d62ef7f4fe70516c63f23f6
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:52 +0100

    Update RestoreCmd.java : refactor NMsg

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/base/cmd/RestoreCmd.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/base/cmd/RestoreCmd.java
@@ -13,7 +13,7 @@ import net.thevpc.ndb.servers.base.NdbCmd;
 import net.thevpc.ndb.servers.base.NdbSupportBase;
 import net.thevpc.ndb.servers.sql.nmysql.util.AtName;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NRef;
 
 import java.io.IOException;

66d153a8825c0e6fb27cc6233b79618011cb48bc
thevpc <taha.bensalah@gmail.com>
2026-01-02 18:51:05 +0100

    Update SqlCreateIndexCmd.java : refactor-element-reader-writer

--- ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/sqlbase/cmd/SqlCreateIndexCmd.java
+++ ndb-servers/src/main/java/net/thevpc/ndb/servers/sql/sqlbase/cmd/SqlCreateIndexCmd.java
@@ -1,7 +1,7 @@
 package net.thevpc.ndb.servers.sql.sqlbase.cmd;
 
+import net.thevpc.nuts.elem.NElementReader;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.elem.NElementParser;
 import net.thevpc.nuts.text.NMsg;
 import net.thevpc.ndb.servers.ExtendedQuery;
 import net.thevpc.ndb.servers.NdbConfig;
@@ -30,7 +30,7 @@ public class SqlCreateIndexCmd<C extends NdbConfig> extends CreateIndexCmd<C> {
             s = s.trim();
             if (s.length() > 0) {
                 if (s.startsWith("{")) {
-                    Map<String, Object> row = NElementParser.ofJson().parse(s, Map.class);
+                    Map<String, Object> row = NElementReader.ofJson().read(s, Map.class);
                     for (Map.Entry<String, Object> e : row.entrySet()) {
                         if (setKeys.length() > 0) {
                             setKeys.append(",");
